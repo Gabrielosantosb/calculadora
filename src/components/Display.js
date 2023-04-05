2e4bc0df03418e391d1728a267a57e17e460f1b3
Gabrielosantosb <98432856+Gabrielosantosb@users.noreply.github.com>
2023-04-05 18:09:29 -0300

    Update Display.js

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,28 +1,55 @@
-import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+// import React from 'react';
+// import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 
-// import { Container } from './styles';
+// // import { Container } from './styles';
 
-export default (props) => {
-  return (
-    <View style={styles.display}>
-      <Text style={styles.displayValue} numberOfLines={1}>
-        {props.value}
-      </Text>
-    </View>
-  );
-};
+// export default (props) => {
+//   return (
+//     <View style={styles.display}>
+//       <Text style={styles.displayValue} numberOfLines={1}>
+//         {props.value}
+//       </Text>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   display: {
+//     flex: 1,
+//     padding: 20,
+//     justifyContent: 'center',
+//     backgroundColor: 'gray',
+//     alignItems: 'flex-end',
+//   },
+//   displayValue: {
+//     color: 'black',
+//     fontSize: 40,
+//   },
+// });
+
+import React from 'react'
+import {
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native'
 
 const styles = StyleSheet.create({
-  display: {
-    flex: 1,
-    padding: 20,
-    justifyContent: 'center',
-    backgroundColor: 'gray',
-    alignItems: 'flex-end',
-  },
-  displayValue: {
-    color: 'black',
-    fontSize: 40,
-  },
-});
+    display: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+        backgroundColor: 'black',
+        alignItems: 'flex-end',
+    },
+    displayValue: {
+        fontSize: 60,
+        color: 'white',
+    }
+})
+
+export default props => 
+    <View style={styles.display}>
+        <Text style={styles.displayValue}
+            numberOfLines={1}>{props.value}</Text>
+    </View>
